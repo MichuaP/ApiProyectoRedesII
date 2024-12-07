690f7705f8e2361e7aa7e3aa3019035095aeceea
Mich <paugam21@outlook.com>
2024-12-07 15:53:03 -0600

    Carpeta y usuarios JSON

--- index.js
+++ index.js
@@ -15,29 +15,8 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-//Api TNYT
-app.get('/nyt', async (req, res) => {
-    const section = req.query.section || 'home'; // Permite filtrar por sección
-    const apiUrl = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.NYT_API_KEY}`;
+//Obtener información del NFS
 
-    try {
-        const response = await axios.get(apiUrl);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching NYT API:', error);
-        res.status(500).json({ error: 'Failed to fetch data from NYT API' });
-    }
-});
+//Subir información del NFS
 
-//Api el país
-app.get('/elpais', async (req, res) => {
-    const apiUrl = 'https://api.elpais.com/...'; 
-
-    try {
-        const response = await axios.get(apiUrl);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching El País API:', error);
-        res.status(500).json({ error: 'Failed to fetch data from El País API' });
-    }
-});
+//Nube
